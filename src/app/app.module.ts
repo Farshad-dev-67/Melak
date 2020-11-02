1d7f8b27fc069a572333fff51755685159c68bb7
Administrator <lari.farshad@gmail.com>
2020-11-03 01:29:46 +0330

    update app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {SidebarModule} from './shared/reusable-module/sidebar/sidebar.module';
+import {HeaderComponent} from './shared/reusable-module/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SidebarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/reusable-module/header/header.component.ts
@@ -0,0 +1,107 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  titleList: Array<any> = [];
+  firstChildItem: Array<any> = [];
+  secondChildItem: Array<any> = [];
+  thirdChildItem: Array<any> = [];
+  forthChildItem: Array<any> = [];
+
+  routeBreadcrumb = [
+    {
+      id: 1,
+      title: 'داشبورد',
+      path: 'dashboard',
+      child: [
+        {
+          id: 11,
+          title: '1 داشبورد',
+          path: 'dashboard',
+        }
+      ]
+    },
+    {
+      id: 2,
+      title: 'فروشگاه',
+      path: 'store'
+    },
+    {
+      id: 3,
+      title: 'پست',
+      path: 'post',
+      child: [
+        {
+          id: 31,
+          title: '1 پست',
+          path: 'post1'
+        },
+        {
+          id: 32,
+          title: '2 پست',
+          path: 'post2'
+        },
+        {
+          id: 33,
+          title: '3 پست',
+          path: 'post3',
+          child: [
+            {
+              id: 113,
+              title: '12 پست',
+              path: 'post2'
+            },
+            {
+              id: 123,
+              title: '32 پست',
+              path: 'post2'
+            },
+            {
+              id: 133,
+              title: '45 پست',
+              path: 'post2',
+              child: [
+                {
+                  id: 213,
+                  title: '67 پست',
+                  path: 'post2'
+                },
+                {
+                  id: 223,
+                  title: '56 پست',
+                  path: 'post2'
+                },
+              ]
+            },
+          ]
+        },
+      ]
+    }
+  ];
+
+  ngOnInit(): void {
+    this.routeBreadcrumb.forEach((res: any) => {
+      if (typeof res.child !== 'undefined') {
+        res.child.forEach((firstChildList) => {
+          this.firstChildItem.push(firstChildList);
+          if (typeof firstChildList.child !== 'undefined') {
+            firstChildList.child.forEach((secondChildList) => {
+              this.secondChildItem.push(secondChildList);
+              if (typeof secondChildList.child !== 'undefined') {
+                secondChildList.child.forEach((thirdChildList) => {
+                  this.thirdChildItem.push(thirdChildList);
+                });
+              }
+            });
+          }
+        });
+      }
+    });
+  }
+
+}
